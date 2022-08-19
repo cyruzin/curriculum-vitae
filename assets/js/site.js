56a95b043446a10695cec6556d8e3bdccedefe0e
Cyro Dubeux <xorycx@gmail.com>
2022-08-19 19:09:20 -0300

    added a hidden contact menu

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,13 +1,16 @@
+// profile click
 const goTo = () => window.open("https://github.com/cyruzin");
 
 const profile = document.getElementById("profile");
 
 profile.addEventListener("click", goTo);
 
+// print section
 const hideMenu = () => {
   document.getElementById("side-menu-left").style.display = "none";
   document.getElementById("certifications").style.display = "none";
   document.getElementById("print").style.display = "none";
+  document.getElementById("contact").style.display = "block";
   window.print();
 };
 
@@ -16,6 +19,7 @@ const print = document.getElementById("print");
 print.addEventListener("click", hideMenu);
 
 const showMenu = () => {
+  document.getElementById("contact").style.display = "none";
   document.getElementById("side-menu-left").style.display = "block";
   document.getElementById("certifications").style.display = "block";
   document.getElementById("print").style.display = "block";
