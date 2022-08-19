3700b181ab4d1e570e43cf40f0189b73eefbce6d
Cyro Dubeux <xorycx@gmail.com>
2022-08-19 18:47:27 -0300

    cv updated / added print button

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,3 +3,22 @@ const goTo = () => window.open("https://github.com/cyruzin");
 const profile = document.getElementById("profile");
 
 profile.addEventListener("click", goTo);
+
+const hideMenu = () => {
+  document.getElementById("side-menu-left").style.display = "none";
+  document.getElementById("certifications").style.display = "none";
+  document.getElementById("print").style.display = "none";
+  window.print();
+};
+
+const print = document.getElementById("print");
+
+print.addEventListener("click", hideMenu);
+
+const showMenu = () => {
+  document.getElementById("side-menu-left").style.display = "block";
+  document.getElementById("certifications").style.display = "block";
+  document.getElementById("print").style.display = "block";
+};
+
+window.onafterprint = showMenu;
